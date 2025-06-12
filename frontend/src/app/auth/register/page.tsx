156fc53074b8b02c62e3ace78043a767770e994d
joungHyeon <jounghyeon123@daum.net>
2025-06-12 21:05:21 +0900

    FE와 BE를 분리

--- frontend/src/app/auth/register/page.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/auth/register/page.tsx
-import { SignUpForm } from "@/components/auth/SignUpForm";
-export default function Register() {
-  return (
-    <div>
-      <SignUpForm />
-    </div>
-  );
-}
