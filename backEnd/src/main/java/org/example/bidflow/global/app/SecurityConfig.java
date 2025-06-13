708ef3426fda220c025b8df6be6e3be4016a8aee
joungHyeon <jounghyeon123@daum.net>
2025-06-13 15:29:20 +0900

    ip 주소 변경

--- backEnd/src/main/java/org/example/bidflow/global/app/SecurityConfig.java
+++ backEnd/src/main/java/org/example/bidflow/global/app/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOrigin("http://localhost:3000");
-        configuration.addAllowedOrigin("http://35.203.149.35:3000");
+        configuration.addAllowedOrigin("http://43.203.21.172:3000");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
